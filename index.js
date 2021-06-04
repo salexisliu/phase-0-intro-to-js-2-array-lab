5b6d72b903337fb5cfa8677a4bcdac7617da2cb2
Alexis S Liu <syl1350@gmail.com>
2021-06-03 23:12:25 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+  cats.push(name);
+  return cats;
+}
+
+function destructivelyPrependCat(name){
+  cats.unshift(name);
+  return cats;
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+  return cats;
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+  return cats;
+}
+
+function appendCat(name) {
+  const copyOfCats = [...cats, name];
+  return copyOfCats;
+}
+
+function prependCat(name) {
+  const copyOfCats = [name, ...cats];
+  return copyOfCats;
+}
+
+function removeLastCat() {
+  return cats.slice(0,-1);
+} 
+
+function removeFirstCat() {
+  return cats.slice(1);
+}
